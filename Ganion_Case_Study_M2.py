d1572c13ed70ac66053e905b5fed1aa020f55cf4
aganion <acganion@gmail.com>
2026-02-03 21:40:26 -0500

    Update Ganion_Case_Study_M2.py

--- Ganion_Case_Study_M2.py
+++ Ganion_Case_Study_M2.py
@@ -9,12 +9,13 @@ user_last_name = input('Enter your last name or "ZZZ" to quit: ')
 if user_last_name != 'ZZZ':
     user_first_name = input('Enter your first name: ')
     user_GPA = float(input('Enter your GPA: '))
+
+    if user_GPA > 3.5:
+        print(f"{user_first_name} {user_last_name} has made the Dean's List")
+    elif user_GPA > 3.2:
+        print(f"{user_first_name} {user_last_name} has made the Honor Roll.")
+    else:
+        print(f"{user_first_name} {user_last_name} has a GPA of {user_GPA}.")
+
 else:
     print('End Program.')
-
-if user_GPA > 3.5:
-    print(f"{user_first_name} {user_last_name} has made the Dean's List")
-elif user_GPA > 3.2:
-    print(f"{user_first_name} {user_last_name} has made the Honor Roll.")
-else:
-    print(f"{user_first_name} {user_last_name} has a GPA of {user_GPA}.")
